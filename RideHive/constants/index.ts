99420576e739dfd26dc756f1bbad7912f21da548
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-11-29 13:12:42 +0530

    onboarding screens details Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

---
+++ RideHive/constants/index.ts
@@ -0,0 +1,26 @@
+export const onboarding = [
+  {
+    id: "1",
+    title: "perfect ride",
+    description: "hlelw",
+    image: null,
+  },
+  {
+    id: "2",
+    title: "pdhdfhdf",
+    description: "hlelw",
+    image: null,
+  },
+  {
+    id: "3",
+    title: "gfdf",
+    description: "hlelw",
+    image: null,
+  },
+  {
+    id: "4",
+    title: "perfect ride",
+    description: "hlelw",
+    image: null,
+  },
+];
