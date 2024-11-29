2d6f657277b582c5b768eca793c0c8eb37590179
W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>
2024-11-29 13:12:24 +0530

    react-native-swiper added for swiper screens Signed-off-by: W A J AMASARA <125728677+WAJAMASARA@users.noreply.github.com>

--- RideHive/app/(auth)/welcome.tsx
+++ RideHive/app/(auth)/welcome.tsx
@@ -1,10 +1,49 @@
-import { Text } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+import Swiper from "react-native-swiper";
+import { useRef, useState } from "react";
+import { onboarding } from "@/constants";
 
 const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
-    <SafeAreaView>
-      <Text>Welcome</Text>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      {/* Skip Button */}
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-red-400 text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      {/* Swiper Component */}
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full " />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View
+            key={item.id} // Add a unique key for each slide
+            className="flex-1 items-center justify-center"
+          >
+            <Text className="text-black text-lg font-JakartaBold">
+              {item.title}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
