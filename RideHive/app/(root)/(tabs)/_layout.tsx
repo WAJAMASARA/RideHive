71120ff90191c8fa097a5fb199900af6988475f3
jiyath amasara <wajamasara@gmail.com>
2024-12-30 16:02:12 +0530

    tab bar completed Signed-off-by: jiyath amasara <wajamasara@gmail.com>

--- RideHive/app/(root)/(tabs)/_layout.tsx
+++ RideHive/app/(root)/(tabs)/_layout.tsx
@@ -11,16 +11,17 @@ const TabIcon = ({
   focused: boolean;
 }) => (
   <View
-    className={`flex flex-row justify-center items-center rounded-full ${focused ? "bg-general-300" : ""}`}
+    className={`flex flex-row justify-center items-center rounded-full mb-8 ${focused ? "bg-general-300" : ""}`}
   >
     <View
-      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? "bg-general-400" : ""}`}
+      className={`rounded-full w-10 h-10 items-center justify-center  ${focused ? "bg-general-400" : ""}`}
     >
       <Image
         source={source}
         tintColor="white"
         resizeMode="contain"
         className="w-7 h-7"
+       
       />
     </View>
   </View>
