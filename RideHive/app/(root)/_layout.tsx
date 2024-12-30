938d6d9d019ce2bc17a3cba073cab384b63b0726
jiyath amasara <wajamasara@gmail.com>
2024-12-30 18:27:39 +0530

    add the zustand dependency Signed-off-by: jiyath amasara <wajamasara@gmail.com>

--- RideHive/app/(root)/_layout.tsx
+++ RideHive/app/(root)/_layout.tsx
@@ -4,9 +4,10 @@ import "react-native-reanimated";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(chat)" options={{ headerShown: false }} />
+      <Stack.Screen name="(rides)" options={{ headerShown: false }} />
+      <Stack.Screen name="(profile)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
