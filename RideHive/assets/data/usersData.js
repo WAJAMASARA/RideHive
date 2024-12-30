66a68a0e3d74c27f7ceb309005074c19e273609c
W A K Jithmini <wajamasara@gmail.com>
2024-12-30 14:58:20 +0530

    add user data file Signed-off-by:  <wajamasara@gmail.com>

---
+++ RideHive/assets/data/usersData.js
@@ -0,0 +1,18 @@
+// usersData.js
+
+export const users = [
+    {
+      id: 1,
+      name: "Jiyath",
+      email: "J@gmail.com",
+      password: "123456",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane@example.com",
+      password: "password456",
+    },
+
+  ];
+  
